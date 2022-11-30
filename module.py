3d984447cfb11f82a5a2772506f6a91a0a00e4a9
junaidsheroz <junaidsheroz213@gmail.com>
2022-11-30 15:58:20 +0530

    junaid

---
+++ module.py
@@ -0,0 +1,23 @@
+def function1():
+    while True:
+        n1 = int(input('Enter number 1: '))
+        n2 = int(input('Enter number 2: '))
+        if n1 == n2:
+            break    
+
+# Ask the User to enter five numbers, store those numbers in ascending order in a single Dimensional array, and print that array
+def function2():
+    # Create a list to hold the numbers
+    numbers = []
+    # Ask the user to enter five numbers
+    for i in range(5):
+        # Ask the user to enter a number
+        number = int(input('Enter a number: '))
+        # Add the number to the list
+        numbers.append(number)
+    # Sort the list
+    numbers.sort()
+    # Print the list
+    print(numbers)
+
+
