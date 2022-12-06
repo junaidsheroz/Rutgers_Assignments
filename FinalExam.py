fc57f2dbbca70a27b40c6e4b6cf45f3c4ce44124
junaidsheroz <junaidsheroz213@gmail.com>
2022-12-06 12:48:11 +0530

    jj

--- FinalExam.py
+++ FinalExam.py
@@ -1,25 +1,29 @@
 # Q1. Get the following output.
 # Create a 2D Array
 
-a = (['A','B'],['F','G','H'])
-b = (['C','D','E'],['I','J'])
+# a = (['A','B'],['F','G','H'])
+# b = (['C','D','E'],['I','J'])
 
-# output
-# J
-# I
-# H
-# F
-# E
-# D
-# C
-# B
-# A
+# # Answer: 
+# for i,j in zip(a[::-1],b[::-1]):
+#     for k in j[::-1]:
+#         print(k)
+#     for l in i[::-1]:
+#         print(l)
 
-# code to print above output
-# solution
+#  Ask total of 5 numbers from the user and store them in the array.
 
-for i,j in zip(a,b):
-    for k in j:
-        print(k,reversed=True)
-    # print(i)
-    # print(j)
+array = []
+for i in range(5):
+    array.append(int(input("Enter a number: ")))
+
+
+print("Highest number is: ", max(array))
+print("Lowest number is: ", min(array))
+
+print('Initial Array - ', array)
+for i in array:
+    if i==max(array) or i==min(array):
+        array.remove(i)
+
+print('After Deleting - ', array)
