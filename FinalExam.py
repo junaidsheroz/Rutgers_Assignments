fa1ec84a8e9126b12e550c091a6396e7714f7633
junaidsheroz <junaidsheroz213@gmail.com>
2022-12-06 15:35:54 +0530

    final

--- FinalExam.py
+++ FinalExam.py
@@ -1,5 +1,4 @@
-# Q1. Get the following output.
-# Create a 2D Array
+# Question 1. 
 
 a = (['A','B'],['F','G','H'])
 b = (['C','D','E'],['I','J'])
@@ -11,12 +10,18 @@ for i,j in zip(a[::-1],b[::-1]):
     for l in i[::-1]:
         print(l)
 
+# output:
 
-
-
-
-
-
+# J
+# I
+# H
+# G
+# F
+# E
+# D
+# C
+# B
+# A
 
 
 # Question 2
@@ -36,6 +41,18 @@ for i in array:
     elif i==min(array):
         array.remove(i)
 print('After Deleting - ', array)
+
+# output:
+
+# Enter a number: 321
+# Enter a number: 43
+# Enter a number: 5
+# Enter a number: 15
+# Enter a number: 34
+# Highest number is:  321
+# Lowest number is:  5
+# Initial Array -  [321, 43, 5, 15, 34]
+# After Deleting -  [43, 15, 34]
 
 # Question 3:
 
@@ -61,6 +78,16 @@ class final(Designation,Emp):
         print('Child Class')
 
 x = final(name='AA',sal = 3000,comm=40,des= 'US')
+
+# Answer:
+
+# 1. The line 1 which is message method declared outside the class is now put in the Emp class
+# 2. The line 4 which calls the method message is now shifted under the class final
+# 3. The Line 8 has been edited such that Destination class comes before Emp.
+# 4. The calling message method has been edited to remove 'self' from it.
+
+
+
 
 
 # Question 4:
@@ -88,6 +115,21 @@ exponent = int(input("Enter a exponent: "))
 print("Exponential Value is: ", base ** exponent)
 
 
+# Ouput:
+
+# 12/06/22
+# Tue
+# Enter a number: 4
+# Square root - 2.0
+# Enter a number: 3
+# Power of 3 - 27
+# Enter a number: 3
+# Exponential - 19.81295094562122
+# Enter a base: 2
+# Enter a exponent: 3
+# Exponential Value is:  8
+
+
 # Question 5:
 a = (['A','B'],['E','F','G'])
 b = (['C','D'],['H','I','K'])
@@ -96,3 +138,16 @@ for i,j in zip(a,b):
     for h,k in zip(i,j):
         print(h)
         print(k)
+
+# Output
+
+# A
+# C
+# B
+# D
+# E
+# H
+# F
+# I
+# G
+# K
